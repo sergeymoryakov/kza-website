055a4023d37c055cd5d4556b27f8778392ceac02
Sergey Moryakov <sergey@moryakov.com>
2023-12-18 21:01:55 +0200

    remove counter app

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import "./components/LinkToggleTouchBlack.js";
 import "./components/LinkIconTouchWhite.js";
 import "./components/LinkHomeWhite.js";
 
-import { setupCounter } from "./counter.js";
+// import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <h4 class="h4">Welcome to Test Counter:</h4>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`;
+// document.querySelector("#app").innerHTML = `
+//   <div>
+//     <h4 class="h4">Welcome to Test Counter:</h4>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//   </div>
+// `;
 
-setupCounter(document.querySelector("#counter"));
+// setupCounter(document.querySelector("#counter"));
 
 // TEST: Button Toggle
 // document.querySelector(".toggle-button").addEventListener("click", function () {
