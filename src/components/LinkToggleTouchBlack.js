a41c69d62294b28f86f988e753936d3cf9f2828a
Sergey Moryakov <sergey@moryakov.com>
2023-12-18 22:30:28 +0200

    tbs css links

--- src/components/LinkToggleTouchBlack.js
+++ src/components/LinkToggleTouchBlack.js
@@ -1,6 +1,6 @@
 const template = document.createElement("template");
 template.innerHTML = `
-            <link rel="stylesheet" href="/components/LinkToggleTouchBlack.css">
+            <link rel="stylesheet" href="./src/components/LinkToggleTouchBlack.css">
             <a class="link-toggle-touch-black" href="#contacts">
                 <span class="button-text">get in touch</span>
                 <span class="toggle-switch"></span>
