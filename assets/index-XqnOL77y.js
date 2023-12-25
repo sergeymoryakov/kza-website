6a50ec7c638511bf182e6632f288d88cd7eb9d9e
Sergey Moryakov <sergey@moryakov.com>
2023-12-25 15:50:48 +0200

    Updates

---
+++ assets/index-XqnOL77y.js
@@ -0,0 +1,77 @@
+(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))n(s);new MutationObserver(s=>{for(const i of s)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function a(s){const i={};return s.integrity&&(i.integrity=s.integrity),s.referrerPolicy&&(i.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?i.credentials="include":s.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(s){if(s.ep)return;s.ep=!0;const i=a(s);fetch(s.href,i)}})();class r extends HTMLElement{connectedCallback(){this.innerHTML=`
+            <button class="btn-toggle-less">
+                <span class="button-text">less</span>
+                <span class="toggle-switch"></span>
+            </button>
+        `}}customElements.define("button-toggle-less",r);class l extends HTMLElement{connectedCallback(){this.innerHTML=`
+            <button class="btn-toggle-more">
+                <span class="button-text">more</span>
+                <span class="toggle-switch"></span>
+            </button>
+        `}}customElements.define("button-toggle-more",l);class d extends HTMLElement{connectedCallback(){this.innerHTML=`
+            <a class="link-toggle-touch-white" href="#contacts">
+                <span class="button-text">get in touch</span>
+                <span class="toggle-switch"></span>
+            </a>
+        `}}customElements.define("link-toggle-touch-white",d);class g extends HTMLElement{connectedCallback(){this.innerHTML=`
+            <a class="link-toggle-touch-black" href="#contacts">
+                <span class="button-text">get in touch</span>
+                <span class="toggle-switch"></span>
+            </a>
+        `}}customElements.define("link-toggle-touch-black",g);class p extends HTMLElement{connectedCallback(){this.innerHTML=`
+            <a class="link-icon-touch-white" href="#contacts">
+                <span class="button-text">get in touch</span>
+                <span class="arrow-right"></span>
+            </a>
+        `}}customElements.define("link-icon-touch-white",p);class u extends HTMLElement{connectedCallback(){this.innerHTML=`
+            <a class="link-home-white" href="#contacts">
+                <span class="button-text">up to home</span>
+            </a>
+        `}}customElements.define("link-home-white",u);class m extends HTMLElement{static get observedAttributes(){return["title","icon","image","text"]}attributeChangedCallback(e,a,n){switch(e){case"title":this._title=n;break;case"icon":this._icon=n;break;case"image":this._image=n;break;case"text":this._text=JSON.parse(n);break}}connectedCallback(){this.classList.add("card-container"),this.innerHTML=`
+                    <div
+                        class="card"
+                        onclick="this.classList.toggle('flipped')"
+                    >
+                        <div class="card-front"
+                            style="background-image: url('${this._image}');"
+                        >
+                            <div class="card-front__top-grid">
+                                <h4 class="h4">${this._title}</h4>
+                                <img
+                                    class="card-icon"
+                                    src="${this._icon}"
+                                    alt=""
+                                />
+                            </div>
+                            <button-toggle-more></button-toggle-more>
+                        </div>
+                        <div class="card-back">
+                            <div class="card-back__top-grid">
+                                <h4 class="h4 ft-light">
+                                    ${this._title}
+                                </h4>
+                                <img
+                                    class="card-icon"
+                                    src="${this._icon}"
+                                    alt=""
+                                />
+                            </div>
+
+                            <div class="card-back__bottom-flex">
+                                <div class="card-button-wrapper">
+                                    <button-toggle-less></button-toggle-less>
+                                </div>
+                                <div class="card-items">
+                                    ${this._text.map(e=>`<p class="p-body ft-light">${e}</p>`).join("")}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        `}}customElements.define("card-container",m);const c=[{id:1,title:"Research, Identification, and Prospecting",icon:"3-copter.svg",image:"1-card.png",text:["Our team specializes in developing and executing targeted marketing, mailing, and direct contact campaigns focused on new sales prospects.","We help you envision and strategically plan your market approach, considering long-term perspectives and product line development."]},{id:2,title:"Customer Relationship Management",icon:"1-earth.svg",image:"2-card.png",text:["We maintain a meticulous database of prospecting activities, customer contacts, and sales opportunities.","Our system ensures seamless access and handoff of accounts to the responsible Regional Sales Directors (RSDs) based on the sales territory and timing."]},{id:3,title:"Effective Communication",icon:"4-communication.svg",image:"3-card.png",text:["Our team engages in cold-calling, prepares and sends materials requested via hotline calls and web inquiries, and diligently follows up with prospects.","We coordinate these activities with the customer's authorized representative to optimize sales efforts."]},{id:4,title:"Engaging Presentations",icon:"2-presentation.svg",image:"4-card.png",text:["We arrange, prepare, and conduct compelling product presentations that captivate your audience.","Our expertise extends to the preparation and review of marketing materials, proposals, pre-sale agreements, and related documentation before prospect presentations."]},{id:5,title:"Facility and Factory Visits",icon:"5-facility.svg",image:"5-card.png",text:["We offer support for facility and factory visits, ensuring a seamless experience for your potential customers."]},{id:6,title:"In-depth Product Knowledge",icon:"6-product.svg",image:"6-card.png",text:["Our team develops a comprehensive working knowledge of your product's features and benefits, encompassing various areas, including design, construction, systems architecture, performance, and operating costs"]},{id:7,title:"Competitor Analysis",icon:"7-competitor.svg",image:"7-card.png",text:["We analyze competitor products thoroughly, allowing us to effectively communicate your product's strengths and advantages relative to prospect needs."]},{id:8,title:"Market Intelligence",icon:"8-market_int.svg",image:"8-card.png",text:["We assist in obtaining and reporting intelligence on your competitors' activities, including sales, pricing, availability, marketing strategies, and new product development."]},{id:9,title:"Timely Reporting",icon:"9-report.svg",image:"9-card.png",text:["We assist in obtaining and reporting intelligence on your competitors' activities, including sales, pricing, availability, marketing strategies, and new product development."]},{id:10,title:"Industry Engagement",icon:"10-indusrty.svg",image:"10-card.png",text:["We actively participate in various local industry functions, promoting your products and services through product presentations, sponsorships, and hosting events."]},{id:11,title:"Comprehensive Record-Keeping",icon:"11-record.svg",image:"11-card.png",text:["We maintain a comprehensive and clear record of ongoing prospecting activities using our customer-provided CRM and LMT tools. Upon request, we provide regularly scheduled reports to senior management."]},{id:12,title:"Sales Forecasts",icon:"12-salesforcast.svg",image:"12-card.png",text:["We provide sales forecasts for your assigned prospect base as requested, assisting in strategic planning and decision-making."]}];function h(t){let e=0;const a=n=>{e=n,t.innerHTML=`count is ${e}`};t.addEventListener("click",()=>a(e+1)),a(0)}const f=document.getElementById("cardsGrid");console.log(c);c.forEach(t=>{const e=document.createElement("card-container");e.setAttribute("title",t.title),e.setAttribute("icon",`icons/${t.icon}`),e.setAttribute("image",`images/${t.image}`),e.setAttribute("text",JSON.stringify(t.text)),f.appendChild(e)});document.querySelector("#app").innerHTML=`
+  <div>
+    <h4 class="h4">Welcome to Test Counter:</h4>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`;h(document.querySelector("#counter"));
