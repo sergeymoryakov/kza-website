cd72f79c63cecd285c4654d8ff8e6041402b324a
Sergey Moryakov <sergey@moryakov.com>
2023-12-28 13:13:11 +0200

    refactor services header

--- src/index.js
+++ src/index.js
@@ -24,16 +24,16 @@ cardsData.forEach((card) => {
     cardsGrid.appendChild(cardElement);
 });
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <h4 class="h4">Welcome to Test Counter:</h4>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`;
+// document.querySelector("#app").innerHTML = `
+//   <div>
+//     <h4 class="h4">Welcome to Test Counter:</h4>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//   </div>
+// `;
 
-setupCounter(document.querySelector("#counter"));
+// setupCounter(document.querySelector("#counter"));
 
 // TEST: Button Toggle
 // document.querySelector(".toggle-button").addEventListener("click", function () {
