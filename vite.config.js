557577e2af8265ca4eba41ac70bd54393c16c8d5
Sergey Moryakov <112798490+sergeymoryakov@users.noreply.github.com>
2024-01-07 22:33:29 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
     return {
-        base: mode === "gh-pages" ? "/kza-mobile/" : "/",
+        base: mode === "gh-pages" ? "/kza-website/" : "/",
         build: {
             outDir: mode === "production" ? "prod" : "dist",
         },
